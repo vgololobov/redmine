d08d42d06545bec15a14beab2c1efbaaca95fca6
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-09-02 21:59:24 +0200

    Rails3: vendor: acts_as_searchable: scope usage update
    Failure:
    test_search_issue_with_multiple_hits_in_journals(SearchTest) [test/unit/search_test.rb:134]:
    <1> expected but was
    <9>.
    
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %28a%B9PIv%CBiA%2C%B7%A4l%BE%DB%E5%B3%FC%9Ev

--- vendor/plugins/acts_as_searchable/lib/acts_as_searchable.rb
+++ vendor/plugins/acts_as_searchable/lib/acts_as_searchable.rb
@@ -120,10 +120,10 @@ module Redmine
             results = []
             results_count = 0
             
-            with_scope(:find => {:conditions => project_conditions}) do
+            scope.with_scope(:find => {:conditions => project_conditions}) do
               with_scope(:find => find_options) do
-                results_count = scope.count(:all)
-                results = scope.find(:all, limit_options)
+                results_count = count(:all)
+                results = find(:all, limit_options)
               end
             end
             [results, results_count]
