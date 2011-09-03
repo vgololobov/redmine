a484882b7dabc61aa7f7a9d2722d5cca7b68e390
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-09-03 15:43:45 +0200

    Rails3: controller: fix sys_controller
    On Rails3.1:
    Failure:
    test_fetch_changesets_one_project(SysControllerTest) [test/functional/sys_controller_test.rb:67]:
    Expected response to be a <:success>, but was <404>.
    
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %97%29d%86%87%B4%9EK%5BI%02%CBa%FF%F7%05pS%DAt

--- app/controllers/sys_controller.rb
+++ app/controllers/sys_controller.rb
@@ -42,7 +42,8 @@ class SysController < ActionController::Base
   def fetch_changesets
     projects = []
     if params[:id]
-      projects << Project.active.has_module(:repository).find(params[:id])
+      # params[:id] can be either an integer or an identifier
+      projects << Project.find(params[:id], :conditions => Project.active.has_module(:repository).where_values)
     else
       projects = Project.active.has_module(:repository).find(:all, :include => :repository)
     end
