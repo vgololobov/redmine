e6279092d49c3828b8f6cf089602c52e69b9d301
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-09-21 09:00:23 +0900

    Rails3.1: vendor: replace "named_scope" to "scope" at vendor/plugins/acts_as_watchable/lib/acts_as_watchable.rb
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %86%A8%7E%C2x%E1p%00%F3%23%C8%9C%0D%D3%1AKM%DD%CB%07

--- vendor/plugins/acts_as_watchable/lib/acts_as_watchable.rb
+++ vendor/plugins/acts_as_watchable/lib/acts_as_watchable.rb
@@ -14,11 +14,12 @@ module Redmine
           class_eval do
             has_many :watchers, :as => :watchable, :dependent => :delete_all
             has_many :watcher_users, :through => :watchers, :source => :user, :validate => false
-            
-            named_scope :watched_by, lambda { |user_id|
-              { :include => :watchers,
-                :conditions => ["#{Watcher.table_name}.user_id = ?", user_id] }
-            }
+            # Rails 3.1
+            # named_scope :watched_by, lambda { |user_id|
+            scope :watched_by, lambda { |user_id|
+                { :include => :watchers,
+                  :conditions => ["#{Watcher.table_name}.user_id = ?", user_id] }
+                 }
             attr_protected :watcher_ids, :watcher_user_ids
           end
         end
