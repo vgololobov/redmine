4a6aeabddce86ad198bba4a649ed9349ef83f9c8
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-09-27 22:29:31 +0900

    Rails3: mail: model
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %97%DB%8F%9D%00%12%FE%3A%27%B5%3E%EAr1%80%A0%94x%9A%C2

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -15,6 +15,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+class BlackholeMailMessage < Mail::Message
+  def self.deliver
+    false
+  end
+end
+
+class AbortDeliveryError < StandardError
+end
+
 class Mailer < ActionMailer::Base
   layout 'mailer'
   helper :application
@@ -26,6 +35,18 @@ class Mailer < ActionMailer::Base
   include Redmine::I18n
 
   self.prepend_view_path "app/views/mailer"
+
+  def abort_delivery
+    raise AbortDeliveryError
+  end
+
+  def process(*args)
+    begin
+      super *args
+    rescue AbortDeliveryError
+      self.message = BlackholeMailMessage
+    end
+  end
 
   def self.default_url_options
     h = Setting.host_name
@@ -344,10 +365,9 @@ class Mailer < ActionMailer::Base
 
     @author ||= User.current
     if @author.pref[:no_self_notified]
-      attributes[:bcc].delete(@author.mail) if attributes[:bcc]
+      attributes[:to].delete(@author.mail) if attributes[:to]
       attributes[:cc].delete(@author.mail) if attributes[:cc]
     end
-
 
     if Setting.bcc_recipients?
       attributes[:bcc] = [attributes[:to], attributes[:cc]].flatten.compact.uniq
@@ -356,9 +376,9 @@ class Mailer < ActionMailer::Base
     end
 
     set_language_if_valid @initial_language
-    return if (attributes[:to].nil? || attributes[:to].empty?) &&
-              (attributes[:cc].nil? || attributes[:cc] .empty?) &&
-              (attributes[:bcc].nil? || attributes[:bcc].empty?)
+    abort_delivery if (attributes[:to].nil? || attributes[:to].empty?) &&
+                      (attributes[:cc].nil? || attributes[:cc] .empty?) &&
+                      (attributes[:bcc].nil? || attributes[:bcc].empty?)
 
     # Set Message-Id and References
     if attributes[:message_id]
