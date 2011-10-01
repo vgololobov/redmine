38b5fcf987ffbcb69c6a35fd28c0cfa47cc551aa
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-01 23:19:44 +0200

    Plugin generator: remove this outdated language file
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : x%1A%08%8B%CE%29%BA%0D%23%85eo%03%9C%60s%81%2A%DE%D5

--- lib/generators/redmine_plugin/redmine_plugin_generator.rb
+++ lib/generators/redmine_plugin/redmine_plugin_generator.rb
@@ -4,7 +4,6 @@ class RedminePluginGenerator < Rails::Generators::NamedBase
   def generate_plugin
     template 'README.rdoc',          "vendor/plugins/redmine_#{name}/README.rdoc"
     template 'init.rb.erb',          "vendor/plugins/redmine_#{name}/init.rb"
-    template 'en.yml',               "vendor/plugins/redmine_#{name}/lang/en.yml"
     template 'en_rails_i18n.yml',    "vendor/plugins/redmine_#{name}/config/locales/en.yml"
     template 'test_helper.rb.erb',   "vendor/plugins/redmine_#{name}/test/test_helper.rb"
   end
