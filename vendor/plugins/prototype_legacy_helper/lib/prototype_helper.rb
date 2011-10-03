82cf802b1423ed5ae2d0adfccce6a9f7911fa60c
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-03 17:14:08 +0900

    vendor/plugins/prototype_legacy_helper/lib/prototype_helper.rb
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %B3s%D9%81%3D-%2A%C6%C8%AC%E7D%B2%F9%AE%F6%FC%F9%DE%1B

--- vendor/plugins/prototype_legacy_helper/lib/prototype_helper.rb
+++ vendor/plugins/prototype_legacy_helper/lib/prototype_helper.rb
@@ -258,18 +258,19 @@ module PrototypeHelper
   # See FormHelper#form_for for additional semantics.
   def remote_form_for(record_or_name_or_array, *args, &proc)
     options = args.extract_options!
+    options[:html] ||= {} # reverse_merged! later, must not be null
 
     case record_or_name_or_array
     when String, Symbol
       object_name = record_or_name_or_array
     when Array
       object = record_or_name_or_array.last
-      object_name = ActionController::RecordIdentifier.singular_class_name(object)
+      object_name = ActionController::RecordIdentifier.dom_class(object)
       apply_form_for_options!(record_or_name_or_array, options)
       args.unshift object
     else
       object      = record_or_name_or_array
-      object_name = ActionController::RecordIdentifier.singular_class_name(record_or_name_or_array)
+      object_name = ActionController::RecordIdentifier.dom_class(record_or_name_or_array)
       apply_form_for_options!(object, options)
       args.unshift object
     end
