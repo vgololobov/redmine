f39415ed68be8b179f42ec10bc1dd06ebc78e140
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-10-11 10:09:52 +0900

    Rails3.1: db migrate: 062_insert_builtin_roles.rb
    DEPRECATION WARNING: the use of 'guard_protected_attributes' will be removed from the next minor release of rails,
    if you want to bypass mass-assignment security then look into using assign_attributes.
    
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : ta%EB.Psc%EC%7E%11%C1%D3%D0%CAM%92poi%FF

--- db/migrate/062_insert_builtin_roles.rb
+++ db/migrate/062_insert_builtin_roles.rb
@@ -1,11 +1,14 @@
 class InsertBuiltinRoles < ActiveRecord::Migration
   def self.up
+    # rails doesn't know about the previously created builtin attribute without this
+    Role.reset_column_information
+
     nonmember = Role.new(:name => 'Non member', :position => 0)
-    nonmember.send(:attributes=, { :builtin => Role::BUILTIN_NON_MEMBER }, false)
+    nonmember.assign_attributes({ :builtin => Role::BUILTIN_NON_MEMBER }, :without_protection => true )
     nonmember.save
 
     anonymous = Role.new(:name => 'Anonymous', :position => 0)
-    anonymous.send(:attributes=, { :builtin => Role::BUILTIN_ANONYMOUS }, false)
+    anonymous.assign_attributes({ :builtin => Role::BUILTIN_ANONYMOUS }, :without_protection => true )
     anonymous.save
   end
 
