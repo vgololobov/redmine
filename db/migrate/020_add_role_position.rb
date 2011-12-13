7be98ea5bae5ed934a720ce9dd16a5784b294ffe
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-12-13 13:22:49 +0900

    Rails3.1: db: fix db migrate 020_add_role_position.rb
    On Rails 3.1.3.
    
    <pre>
    ==  AddRolePosition: migrating ================================================
    -- add_column(:roles, :position, :integer, {:default=>1})
      -> 0.0170s
    rake aborted!
    An error has occurred, this and all later migrations canceled:
    SQLite3::SQLException: no such column: builtin: SELECT "roles".* FROM "roles"  ORDER BY builtin, position, id
    </pre>
    
    3.0.11 passes.
    
    --HG--
    branch : rails3-straight-20111220-12
    extra : transplant_source : %C6%10ZG0%14%A6X%95c%C1%EF%C4%EEVk%B7P%24%02

--- db/migrate/020_add_role_position.rb
+++ db/migrate/020_add_role_position.rb
@@ -1,7 +1,9 @@
 class AddRolePosition < ActiveRecord::Migration
   def self.up
     add_column :roles, :position, :integer, :default => 1
+    add_column :roles, :builtin, :integer, :default => 0, :null => false
     Role.all.each_with_index {|role, i| role.update_attribute(:position, i+1)}
+    remove_column :roles, :builtin
   end
 
   def self.down
